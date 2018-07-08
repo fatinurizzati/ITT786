0f16115aa27ba10632f40446a142663c72cd0b8c
fatinurizzati <37464038+fatinurizzati@users.noreply.github.com>
2018-07-08 22:54:48 +0800

    Add files via upload

---
+++ Assignment2/Assignment2.java
@@ -0,0 +1,38 @@
+class RunnableDemo implements Runnable {
+   private Thread t;
+   private String threadName;
+   
+   RunnableDemo( String name) {
+      threadName = name;
+      System.out.println("Creating " +  threadName );
+   }
+   
+   public void run() {
+      System.out.println("Running " +  threadName );
+      try {
+         Thread.sleep(50);
+      }catch (InterruptedException e) {
+         System.out.println("Thread " +  threadName + " interrupted.");
+      }
+      System.out.println("Thread " +  threadName + " exiting.");
+   }
+   
+   public void start () {
+      System.out.println("Starting " +  threadName );
+      if (t == null) {
+         t = new Thread (this, threadName);
+         t.start ();
+      }
+   }
+}
+
+public class TestThread {
+
+   public static void main(String args[]) {
+       
+        for(int i = 1; i <= 100; i++) {
+            RunnableDemo R = new RunnableDemo( "Thread-"+ i);
+            R.start();
+         }
+   }   
+}
